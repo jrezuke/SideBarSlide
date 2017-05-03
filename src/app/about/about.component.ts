db1bac9dd57266a1af4b972ec0d7bb5a20b94bbc
jrezuke <j.rezuke@verizon.net>
2017-05-03 18:22:40 -0400

    jquery-ui

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+declare var jQuery: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
-
+export class AboutComponent implements OnInit, AfterViewInit {
+  @ViewChild('btnJq') btnJq: ElementRef;
+  @ViewChild('input') input: ElementRef;
+  @ViewChild('sn') sn:ElementRef;
+  
   constructor() { }
 
   ngOnInit() {
+    console.log("btnJq", jQuery(this.btnJq.nativeElement));
+    console.log("#btnJq", jQuery("#btnJq"));
+    jQuery("#btnJq").click(function(){alert("clicked")});
   }
 
+  ngAfterViewInit() {
+    jQuery(this.input.nativeElement).datepicker();
+  }
+
+  onToggle() {
+        
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { trigger, state, animate, transition, style } from '@angular/animations';
 import { SideBarVisibilityService } from "app/services/side-bar-visibility.service";
+declare var jQuery: any;
 
 @Component({
   selector: 'app-home',
@@ -14,21 +15,51 @@ import { SideBarVisibilityService } from "app/services/side-bar-visibility.servi
         ])
   ]
 })
-export class HomeComponent implements OnInit {
+
+export class HomeComponent implements OnInit, AfterViewInit {
   state = "show";
+  width = 300;
+  //origWidth = 0;
+  //sbLeft = this.width - 8;
   buttonText = "<";
-  constructor(private _sbvService: SideBarVisibilityService) { }
+  @ViewChild('sb') sb: ElementRef;
+  @ViewChild('sbc') sbc: ElementRef;
+  @ViewChild('input') input: ElementRef;
+
+  constructor(private _sbvService: SideBarVisibilityService,
+    private _cd: ChangeDetectorRef) { }
 
   ngOnInit() {
     this._sbvService.state$.subscribe((state) => {      
         this.state = state;  
-        console.log('HomeSideBarComponent.ngOnInit state', this.state);    
+        console.log('HomeSideBarComponent.ngOnInit state', this.state);
+                     
     });
+
   }
 
-  onDragStart(event){
+  ngAfterViewInit(){
+    let that = this;
+    //this.origWidth = this.width
+    jQuery(this.sb.nativeElement).draggable({
+      axis: "x",
+      containment: "parent",
+      drag: function(event, ui){
+        //console.log("dragging: ", event, ui);
+        //console.log("sb right: ", this.style.left);
+        //console.log(that.sbc.nativeElement);
+        //console.log("ui.position.left", ui.position.left);
+        //that.width = that.origWidth + ui.position.left;
+        //this._cd.markForCheck();
+      }
+    });
+    jQuery(this.input.nativeElement).datepicker();
+  }
+
+  onDrag(event){
     //event.preventDefault();
     console.log("onDragStart:", event);
+
   }
 
   onDrop(event){
@@ -39,6 +70,7 @@ export class HomeComponent implements OnInit {
   allowDrop(event){
     event.preventDefault();
     console.log("allowDrop:", event);
+    console.log("allowDrop:", event.dataTransfer);
   }
 
   onSideBarToggle(){
